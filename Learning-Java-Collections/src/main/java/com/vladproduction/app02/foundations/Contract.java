a1883ea5cae808c7a990e07f2b4ee23cdca6944a
VladProduction <124387608+vladproduction@users.noreply.github.com>
2025-05-26 20:10:32 +0300

    Delete Learning-Java-Collections directory

--- Learning-Java-Collections/src/main/java/com/vladproduction/app02/foundations/Contract.java
+++
@@ -1,7 +0,0 @@
-package com.vladproduction.app02.foundations;
-
-public interface Contract extends ExtendedContract{
-    //abstract type
-    void term1();
-    void term2();
-}
--- Learning-Java-Collections/src/main/java/com/vladproduction/app02/foundations/ExtendedContract.java
+++
@@ -1,7 +0,0 @@
-package com.vladproduction.app02.foundations;
-
-public interface ExtendedContract {
-
-    void extendedTerm();
-
-}
